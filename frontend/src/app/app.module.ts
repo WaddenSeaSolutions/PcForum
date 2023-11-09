96b4f77970a8c2d2b3a13f7905e2a82efe1214e9
jonasjakobsen1152 <114471902+jonasjakobsen1152@users.noreply.github.com>
2023-11-09 10:42:49 +0100

    App module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,10 +6,14 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import {HomePage} from "./home/home.page";
+import {LoginPageComponent} from "./login-page/login-page.component";
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  declarations: [AppComponent, LoginPageComponent],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, FormsModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- frontend/src/app/login-page/login-page.component.ts
+++ frontend/src/app/login-page/login-page.component.ts
@@ -1,19 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl} from "@angular/forms";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-login-page',
   template:
     `
-    <ion-input placeholder="Username"></ion-input>
-    <ion-input placeholder="Password"></ion-input>
-    <ion-button>Login</ion-button>
+    <ion-input placeholder="Username" [ngModel]="username"></ion-input>
+    <ion-input placeholder="Password" [ngModel]="password"></ion-input>
+    <ion-button (click)="login()">Login</ion-button>
     `,
   styleUrls: ['./login-page.component.scss'],
 })
-export class LoginPageComponent  implements OnInit {
+export class LoginPageComponent {
+  username = new FormControl('')
+  password = new FormControl('')
 
-  constructor() { }
+  myFormGroup = new FormControl({
+    username: this.username,
+    password: this.password,
+  })
+  constructor(private http: HttpClient) { }
 
-  ngOnInit() {}
-
+async login(){
+   // const call = this.http.post()
+}
 }
