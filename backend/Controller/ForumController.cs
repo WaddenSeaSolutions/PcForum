5ba53a215011dcf5d0630cc73a327230349b97f5
Andreas Eriksen <pjuske.10@live.dk>
2023-11-09 13:20:17 +0100

    Register is up

--- backend/Controller/ForumController.cs
+++ backend/Controller/ForumController.cs
@@ -1,6 +1,25 @@
+using backend.Model;
+using backend.Service;
+using Microsoft.AspNetCore.Mvc;
+
 namespace backend.Controller;
 
-public class ForumController
+[ApiController]
+public class ForumController : ControllerBase
 {
+    private readonly ForumService _forumService;
+
+    public ForumController(ForumService forumService)
+    {
+        _forumService = forumService;
+    }
+
+    [HttpPost]
+    [Route("/register")]
+    public User Register([FromBody] User user)
+    {
+        return _forumService.Register(user);
+    }
+    
     
 }
