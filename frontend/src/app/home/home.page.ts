3979de8a991b27a70599a8eac9bad51994c7dc26
Thomasmp00 <thomaxx00@gmail.com>
2023-11-09 13:20:25 +0100

    Homepage frontend 0,1

--- frontend/src/app/home/home.page.ts
+++ frontend/src/app/home/home.page.ts
@@ -2,7 +2,22 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  template: `` ,
+  template: `
+    <body>
+    <header>
+      <h1>rofl forum</h1>
+    </header>
+    <div id="toolbarHeader">
+      <ion-item>
+        <ion-buttons><u>Forside</u></ion-buttons>
+        <ion-buttons style = "margin-left: 90%"><u>Profil</u></ion-buttons>
+        <ion-buttons style = "margin-left: 1%"><u>Log ud</u></ion-buttons>
+      </ion-item>
+
+
+    </div>
+    </body>
+  ` ,
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
