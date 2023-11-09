bd0fc27e9c8a9e90af832fc5c0fe9380eae0f075
Andreas Eriksen <pjuske.10@live.dk>
2023-11-09 11:22:25 +0100

    swagger up

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,6 +1,24 @@
+using backend;
+
 var builder = WebApplication.CreateBuilder(args);
+
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddNpgsqlDataSource(Utilities.ProperlyFormattedConnectionString,
+        dataSourceBuilder => dataSourceBuilder.EnableParameterLogging());
+}
+
+if (builder.Environment.IsProduction())
+{
+    builder.Services.AddNpgsqlDataSource(Utilities.ProperlyFormattedConnectionString);
+}
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 app.Run();
