8b0e0dce26864619c4234b1d805031b2d320c315
Thomasmp00 <thomaxx00@gmail.com>
2023-11-13 11:48:44 +0100

    Cool navigation bar

--- frontend/src/app/home/home.page.ts
+++ frontend/src/app/home/home.page.ts
@@ -5,16 +5,33 @@ import { Component } from '@angular/core';
   template: `
     <body>
     <header>
-      <h1>rofl forum</h1>
+      <h1 id="mainHeadline">PC Forum</h1>
     </header>
     <div id="toolbarHeader">
-      <ion-item>
-        <ion-buttons><u>Forside</u></ion-buttons>
-        <ion-buttons style = "margin-left: 90%"><u>Profil</u></ion-buttons>
-        <ion-buttons style = "margin-left: 1%"><u>Log ud</u></ion-buttons>
+      <ion-item id="toolbarContent">
+        <ion-buttons>
+          <ion-icon id = "icons" name="home"></ion-icon>
+          <p>Forside</p>
+        </ion-buttons>
+
+        <ion-icon style = "margin-left: 75%" name="search"></ion-icon>
+        <ion-input> </ion-input>
+
+        <ion-buttons>
+          <ion-icon id = "icons" name="notifications"></ion-icon>
+          <p>9+</p>
+        </ion-buttons>
+        <ion-buttons style = "margin-left: 0.6%">
+          <ion-icon id = "icons" name="person"></ion-icon>
+          <p>Profil</p>
+        </ion-buttons>
+        <ion-buttons style = "margin-left: 0.6%">
+          <ion-icon id = "icons" name="exit-sharp"></ion-icon>
+          <p>Log ud</p>
+        </ion-buttons>
+
+
       </ion-item>
-
-
     </div>
     </body>
   ` ,
