3995ee14ddcadc59a8aefe415d38efa5eaaeb874
Thomasmp00 <thomaxx00@gmail.com>
2023-11-13 13:10:50 +0100

    button fix

--- frontend/src/app/home/home.page.ts
+++ frontend/src/app/home/home.page.ts
@@ -10,22 +10,30 @@ import { Component } from '@angular/core';
     <div id="toolbarHolder">
     <ion-toolbar id="toolbarHeader">
       <ion-item id="toolbarContent">
-        <ion-buttons>
+        <ion-buttons id="ionButton">
+          <ion-button>
           <ion-icon id = "icons" name="home"></ion-icon>
           <p>Forside</p>
+          </ion-button>
         </ion-buttons>
         <ion-searchbar id = "searchBar"> </ion-searchbar>
-        <ion-buttons>
+        <ion-buttons id="ionButton">
+          <ion-button>
           <ion-icon id = "icons" name="notifications"></ion-icon>
           <p>9+</p>
+          </ion-button>
         </ion-buttons>
-        <ion-buttons style = "margin-left: 0.6%">
+        <ion-buttons id="ionButton" style = "margin-left: 0.6%">
+          <ion-button>
           <ion-icon id = "icons" name="person"></ion-icon>
           <p>Profil</p>
+          </ion-button>
         </ion-buttons>
-        <ion-buttons style = "margin-left: 0.6%">
+        <ion-buttons id="ionButton" style = "margin-left: 0.6%">
+          <ion-button>
           <ion-icon id = "icons" name="exit-sharp"></ion-icon>
-          <p>Log_ud</p>
+          <p>Log ud</p>
+          </ion-button>
         </ion-buttons>
 
       </ion-item>
