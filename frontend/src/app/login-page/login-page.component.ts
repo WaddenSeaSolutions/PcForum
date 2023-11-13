ee9d082faba8e7c20077ef4881afa72d18dcba9e
jonasjakobsen1152 <114471902+jonasjakobsen1152@users.noreply.github.com>
2023-11-13 13:34:06 +0100

    color coding

--- frontend/src/app/login-page/login-page.component.ts
+++ frontend/src/app/login-page/login-page.component.ts
@@ -10,7 +10,7 @@ import {Router} from "@angular/router";
     `
       <div class="background"></div>
       <ion-content style="--background: none; position: absolute; top: 40%">
-        <div style="margin-left: 30%; margin-right: 30%">
+        <div style="margin-left: 20%; margin-right: 20%">
           <ion-item>
             <ion-input style="text-align: center" placeholder="Brugernavn" [formControl]="username"></ion-input>
           </ion-item>
@@ -19,10 +19,11 @@ import {Router} from "@angular/router";
             <ion-input style="text-align: center" placeholder="Kodeord" [formControl]="password"></ion-input>
           </ion-item>
           <br>
-          <ion-button (click)="login()" style="display: flex">Login</ion-button>
-          <br>
-          <ion-button (click)="registerNewUser()" style="display: flex">Opret Konto</ion-button>
-        </div>
+          <div>
+          <ion-button class="btnBackground" (click)="login()">Login</ion-button>
+          <ion-button class="btnBackground" (click)="registerNewUser()">Opret Konto</ion-button>
+          </div>
+          </div>
       </ion-content>
     `,
   styleUrls: ['./login-page.component.scss'],
--- frontend/src/app/register/register.component.ts
+++ frontend/src/app/register/register.component.ts
@@ -1,55 +1,83 @@
 import { Component} from '@angular/core';
 import {FormControl, Validators} from "@angular/forms";
 
+
+
 @Component({
   selector: 'register',
   template:
-    `
+    `<div class="background"></div>
+    <ion-content style="--background: none; position: absolute; top: 40%">
+        <div style="margin-left: 20%; margin-right: 20%;">
+          <header style="text-align: center; font-size: 20px"> Registrer en konto</header>
+          <br>
+          <ion-item>
+            <ion-input style="text-align: center" placeholder="Email" [formControl]="email"> </ion-input>
+          </ion-item>
+          <div *ngIf="email.hasError('minlength')">
+            <p>Email skal være over 3 tegn</p>
+          </div>
+          <div *ngIf="email.hasError('maxlength')">
+            <p>Email skal være under 30 tegn</p>
+          </div>
+          <br>
 
-      <ion-content style="--background: none; position: absolute; top: 40%">
-      <div style="margin-left: 20%; margin-right: 20%;">
-        <header style="text-align: center; font-size: 20px"> Registrer en konto</header>
-        <br>
-        <ion-item>
-    <ion-input style="text-align: center" placeholder="Email" [formControl]="email"> </ion-input>
-    </ion-item>
-      <br>
-      <ion-item>
-      <ion-input style="text-align: center" placeholder="Brugernavn" [formControl]="username"> </ion-input>
-      </ion-item>
-      <br>
-      <ion-item>
-      <ion-input style="text-align: center" placeholder="Kodeord" [formControl]="password"> </ion-input>
-      </ion-item>
-      <br>
-      <ion-item>
-      <ion-input style="text-align: center" placeholder="Gentag Kodeord" [formControl]="password2"> </ion-input>
-    </ion-item>
-    <br>
-        <ion-button style="display: flex">Registrer din konto</ion-button>
-      </div>
-    </ion-content>
+          <ion-item>
+            <ion-input style="text-align: center" placeholder="Brugernavn" [formControl]="username"> </ion-input>
+          </ion-item>
+          <div *ngIf="username.hasError('minlength')">
+            <p>Brugernavn skal være mindst 5 tegn</p>
+          </div>
+          <div *ngIf="username.hasError('maxlength')">
+            <p>Brugernavn skal være højest 20 tegn</p>
+          </div>
+
+          <br>
+          <ion-item>
+            <ion-input type="password" style="text-align: center" placeholder="Kodeord" [formControl]="password"> </ion-input>
+          </ion-item>
+          <br>
+          <ion-item>
+            <ion-input type="password" style="text-align: center" placeholder="Gentag Kodeord" [formControl]="password2"> </ion-input>
+          </ion-item>
+          <div *ngIf="password2.hasError('required')">
+            <p>Password confirmation is required</p>
+          </div>
+          <div *ngIf="password2.hasError('passwordsNotMatch')">
+            <p>Passwords do not match</p>
+          </div>
+          <br>
+          <ion-button class="btnBackground" style="display: flex" [disabled]="myFormGroup.invalid">Registrer din konto</ion-button>
+        </div>
+      </ion-content>
+
     `,
   styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent{
-  email = new FormControl('',Validators.compose([Validators.min(5),Validators.max(30)]));
-  username = new FormControl('',Validators.compose([Validators.min(5),Validators.max(20)]));
-  password = new FormControl('',Validators.compose([Validators.min(8),Validators.max(30)]));
-  password2 = new FormControl('', Validators.required)
+  email = new FormControl('',[Validators.required, Validators.maxLength(30),Validators.minLength(5)]);
+  username = new FormControl('',[Validators.required, Validators.minLength(5),Validators.maxLength(20)]);
+  password = new FormControl('',[Validators.required, Validators.minLength(8),Validators.maxLength(30)]);
+  password2 = new FormControl('', [Validators.required, this.matchingPasswords.bind(this)]);
 
   myFormGroup = new FormControl({
     email: this.email,
     username: this.username,
     password: this.password,
-    password2: this.password2
+    password2: this.password2,
   });
 
-  private passwordMatchValidator(){
-
+  // Method that validates that the password matches and ensures no typos in password
+  matchingPasswords(control: FormControl): { [key: string]: boolean } | null {
+    if (this.password && control.value !== this.password.value) {
+      return { 'passwordsNotMatch': true };
+    }
+    return null;
   }
+
   constructor() { }
 
+// Method to register the new user
   async registerUser(){
     const registrant = {
       email: this.email,
@@ -66,3 +94,4 @@ export interface UsersRegister {
   username: string;
   password: string;
 }
+
