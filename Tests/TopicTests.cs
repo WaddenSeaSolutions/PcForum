5260a326a67a333fbba044f91a8d38a2f9a6f51e
Thomasmp00 <thomaxx00@gmail.com>
2023-11-21 10:54:13 +0100

    First test

--- Tests/TopicTests.cs
+++ Tests/TopicTests.cs
@@ -1,38 +1,51 @@
 
 
+using System.Data;
 using System.Net.Http.Json;
+using Dapper;
+using FluentAssertions;
 using Newtonsoft.Json;
 
 namespace Tests;
 
 public class Tests
 {
-    [SetUp]
-    public void Setup()
-    {
-    }
 
-    [TestCase("TestTitle","TestImage")]
-    public async Task topicCreationTest(string title, string image)
+    [TestCase("TestTitle", "TestImage", false)]
+    public async Task topicCreationTest(string title, string image, Boolean deleted)
     {
-        //Arrange
+        // Arrange
         var testTopic = new Topic()
         {
-            id = 1,
+            deleted = deleted,
             title = title,
             image = image
         };
-        
-        //Act
+
+        // Act: Send POST request to create a topic
         var httpResponse = await new HttpClient().PostAsJsonAsync(Helper.ApiBaseUrl + "topics", testTopic);
         var json = await httpResponse.Content.ReadAsStringAsync();
-        Topic topic = JsonConvert.DeserializeObject<Topic>(json);
         
+
+        // Assert: Retrieve data from the database and compare
         await using (var conn = Helper.DataSource.OpenConnection())
         {
-            var query = $@"SELECT topic.id as {nameof(Topic.id)},
-                title as {nameof(Topic.title)}";
-        }
+            var query = $@"SELECT id as {nameof(Topic.id)},
+            title as {nameof(Topic.title)},
+            image as {nameof(Topic.image)}
+            from forum.topics
+            ORDER BY id DESC;";
 
+            var exp = conn.QueryFirstOrDefault<Topic>(query); // Use QueryFirstOrDefault instead of QueryFirst
+            // Log the retrieved 'exp' for further inspection
+            Console.WriteLine($"Retrieved topic from database: {JsonConvert.SerializeObject(exp)}");    
+
+
+           
+
+            // Compare the properties of topic and exp
+            exp.Should().BeEquivalentTo(testTopic, options => options.Excluding(o => o.id));
+        }
     }
+
 }
