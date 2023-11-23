42ca489f714dac2c0342bba2a33deb73ce70f292
jonasjakobsen1152 <114471902+jonasjakobsen1152@users.noreply.github.com>
2023-11-23 09:29:41 +0100

    Validators in c#

--- backend/Model/User.cs
+++ backend/Model/User.cs
@@ -7,12 +7,19 @@ public class User
     public int Id { get; set; }
     
     [Required]
+    [MinLength(5)]
+    [StringLength(20)]
     public  string Username { get; set; }
     
     [Required]
+    [MinLength(8)]
+    [StringLength(50)]
     public string Password { get; set; }
     
     [Required]
+    [MinLength(5)]
+    [StringLength(30)]
+    [EmailAddress]
     public string Email { get; set; }
     
     public string UserRole  {get; set; }
