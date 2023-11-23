39e9cffb356e2e9d864a5828c65f44ba95c280cc
Andreas Eriksen <pjuske.10@live.dk>
2023-11-23 10:41:24 +0100

    hej

--- frontend/src/app/topic/topic.component.ts
+++ frontend/src/app/topic/topic.component.ts
@@ -11,20 +11,17 @@ import {FormControl} from "@angular/forms";
   selector: 'topic',
   template: `
     <ion-content style="--background: none; top: 20%">
-      <ion-card>
-        <ion-button class="btnBackground" (click)="createNewThread()" style="background: none;">Opret</ion-button>
-      </ion-card>
-      <div *ngFor="let thread of service.threads">
-        <ion-card id="threadCard">
-         <ion-title (click)="openThread(thread)" style="color: white; cursor: pointer"> {{thread.title}} </ion-title>
-        </ion-card>
+      <div style="background: #1e1e1e; padding: 2%; margin-left: 10%; margin-right: 10%; border: 1px solid grey">
+        <ion-item style="border: 1px solid grey">
+          <u (click)="createNewThread()" style="cursor: pointer; color: white; ">Opret</u>
+        </ion-item>
+        <div *ngFor="let thread of service.threads">
+          <ion-card id="threadCard" style="--background: none; background: black">
+            <ion-title (click)="openThread(thread)" style="color: white; cursor: pointer"> {{thread.title}} </ion-title>
+          </ion-card>
+        </div>
       </div>
-
     </ion-content>
-
-
-
-
   `,
   styleUrls: ['./topic.component.scss'],
 })
@@ -35,9 +32,12 @@ export class TopicComponent {
   }
 
   async getThreads() {
+    this.route.params.subscribe(async (params) => {
+      const topicId = params['id'];
 
-    const call = this.http.get<Thread[]>(environment.baseUrl + '/threads/', topicId);
-    this.service.threads = await firstValueFrom<Thread[]>(call);
+      const call = this.http.get<Thread[]>(`${environment.baseUrl}/threads/${topicId}`);
+      this.service.threads = await firstValueFrom<Thread[]>(call);
+    });
   }
 
   async openThread(thread: Thread) {
@@ -49,7 +49,5 @@ export class TopicComponent {
       const topicId = params['id']
     this.router.navigate(['thread-creation', topicId ])});
   }
-
-
 
 }
