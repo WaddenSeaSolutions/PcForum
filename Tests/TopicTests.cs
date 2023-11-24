925da26eeb0f28cabaebcabba9379c9d3631fdbb
Thomasmp00 <thomaxx00@gmail.com>
2023-11-24 10:20:59 +0100

    delete test

--- Tests/TopicTests.cs
+++ Tests/TopicTests.cs
@@ -48,6 +48,60 @@ public class Tests
         }
     }
     
+    [Test] 
+    public async Task CreateThenDeleteTopicTest() 
+    { 
+        var testTopic = new Topic()
+        {
+            deleted = false,
+            title = "TestTitle",
+            image = "TestImage"
+        };
+
+        // HttpClient for API requests
+        var client = new HttpClient();
+
+        // API POST request to create a new topic
+        //...
+        var httpResponse = await client.PostAsJsonAsync(Helper.ApiBaseUrl + "topics", testTopic);
+        httpResponse.EnsureSuccessStatusCode();  
+
+// Separate query to fetch the topic with the highest ID
+        await using (var conn = Helper.DataSource.OpenConnection())
+        {
+            var query = @"SELECT id FROM forum.topics ORDER BY id DESC LIMIT 1;";
+            var createdTopicId = conn.QueryFirstOrDefault<int>(query); 
+
+            var responseBody = await httpResponse.Content.ReadAsStringAsync();
+            Console.WriteLine(responseBody); 
+            var createdTopic = JsonConvert.DeserializeObject<Topic>(responseBody);
+            
+//...
+
+            // API PUT request to delete the topic
+            // API PUT request to delete the topic
+            httpResponse = await client.PutAsync(Helper.ApiBaseUrl + "topics/" + createdTopicId, null);
+
+            if (!httpResponse.IsSuccessStatusCode)
+            {
+                var errorResponse = await httpResponse.Content.ReadAsStringAsync();
+                throw new Exception($"Request failed with status code {httpResponse.StatusCode}. Response: {errorResponse}");
+            }
+
+            httpResponse.EnsureSuccessStatusCode();
+
+            // Assert: Retrieve the deleted flag from the database and compare
+            
+                var query2 = $@"SELECT deleted as {nameof(Topic.deleted)}
+        FROM forum.topics
+        WHERE id = @id;";
+
+                var deleted = conn.QueryFirstOrDefault<bool>(query2, new { id = createdTopicId });
+
+                Assert.IsTrue(deleted);
+        }
+    }
+    
     
 
 }
