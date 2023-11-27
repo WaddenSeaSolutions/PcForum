f7777958befdb4b0e6f6878f467922d3250abb23
Andreas Eriksen <pjuske.10@live.dk>
2023-11-27 11:15:28 +0100

    openThread

---
+++ frontend/src/app/thread-detail/thread-detail.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-thread-detail',
+  template: `
+
+  `,
+  styleUrls: ['./thread-detail.component.scss'],
+})
+export class ThreadDetailComponent {
+
+  constructor() { }
+
+}
