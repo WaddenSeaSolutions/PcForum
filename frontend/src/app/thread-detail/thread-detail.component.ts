7ca37dbcd9884353e7662db97d37a73374ec483e
Andreas Eriksen <pjuske.10@live.dk>
2023-11-27 11:59:26 +0100

    OPenThread

--- frontend/src/app/thread-detail/thread-detail.component.ts
+++ frontend/src/app/thread-detail/thread-detail.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Service} from "../../Service";
+import {ActivatedRoute, Router} from "@angular/router";
+import {Thread} from "../../Interface";
+import {environment} from "../../environments/environment";
+import {firstValueFrom} from "rxjs";
 
 @Component({
   selector: 'app-thread-detail',
@@ -9,6 +15,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ThreadDetailComponent {
 
-  constructor() { }
+  constructor(private http: HttpClient, public service: Service, private route: ActivatedRoute, private router: Router) {
+    this.getThread();
 
+  }
+  async getThread() {
+    this.route.params.subscribe(async (params) => {
+      const threadId = params['id'];
+
+      const call = this.http.get<Thread[]>(`${environment.baseUrl}/thread/${threadId}`);
+      this.service.threads = await firstValueFrom<Thread[]>(call);
+    });
+  }
 }
