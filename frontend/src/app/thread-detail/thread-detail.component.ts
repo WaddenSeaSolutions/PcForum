5a2edae4db839d7c095e13c9c1c21a488f85d00e
AndreasHEriksen <pjuske.10@live.dk>
2023-11-29 09:29:19 +0100

    more thread

--- frontend/src/app/thread-detail/thread-detail.component.ts
+++ frontend/src/app/thread-detail/thread-detail.component.ts
@@ -10,9 +10,9 @@ import {firstValueFrom} from "rxjs";
   selector: 'app-thread-detail',
   template: `
     <ion-content style="--background: none; top: 20%">
-      <div style="background: #1e1e1e; padding: 2%; margin-left: 10%; margin-right: 10%; border: 1px solid grey" *ngIf="thread">
-        <p style="color: white;">Title: {{thread.title}}</p>
-        <p style="color: white;">Body: {{thread.body}}</p>
+      <div style="background: #1e1e1e; padding: 2%; margin-left: 10%; margin-right: 10%; border: 1px solid grey">
+        <p style="color: white;">Title: {{service.thread}}</p>
+        <p style="color: white;">Body: {{service.thread}}</p>
         <ion-button id="DeleteButton">Delete this thread</ion-button>
         <ion-item style="border: 1px solid grey">
 
@@ -23,25 +23,18 @@ import {firstValueFrom} from "rxjs";
   styleUrls: ['./thread-detail.component.scss'],
 })
 export class ThreadDetailComponent {
-  thread: Thread | undefined;
 
   constructor(private http: HttpClient, public service: Service, private route: ActivatedRoute, private router: Router) {
     this.getThread();
 
   }
+
   async getThread() {
-    this.route.params.subscribe((params) => {
+    this.route.params.subscribe(async (params) => {
       const threadId = params['id'];
-      this.http.get<Thread>(`http://localhost:5000/thread/${threadId}`).toPromise()
-        .then(
-          (response) => {
-            console.log(response);
-            this.thread = response;
-          },
-          (error) => {
-            console.error('Error fetching thread details:', error);
-          }
-        );
+
+      const call = this.http.get<Thread>(`${environment.baseUrl}/thread/${threadId}`);
+      this.service.thread = await firstValueFrom<Thread>(call);
     });
   }
 }
