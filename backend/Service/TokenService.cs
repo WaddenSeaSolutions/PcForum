10badb025514cfaab9a1131b4fd27180bcf3354e
unknown <jonasjakobsen1152@gmail.com>
2023-12-04 19:58:35 +0100

    Update TokenService.cs

--- backend/Service/TokenService.cs
+++ backend/Service/TokenService.cs
@@ -14,11 +14,12 @@ public class TokenService
 {
     private TokenDal _tokenDal;
 
-    private static readonly byte[] Secret = Encoding.UTF8.GetBytes(Environment.GetEnvironmentVariable("jwtKey")!);
+    private static byte[] Secret;
 
     public TokenService(TokenDal tokenDal)
     {
         _tokenDal = tokenDal;
+        Secret = Encoding.UTF8.GetBytes(Environment.GetEnvironmentVariable("jwtKey")!);
     }
 
     public string CreateToken(User user)
