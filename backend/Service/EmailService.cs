db68b5e6582f42c28d338400f5da284b38d8d838
jonasjakobsen1152 <114471902+jonasjakobsen1152@users.noreply.github.com>
2023-12-15 10:43:57 +0100

    More exeption handling

--- backend/Service/EmailService.cs
+++ backend/Service/EmailService.cs
@@ -9,28 +9,36 @@ public class EmailService
     
     public void sendEmail(User user)
     {
-        var message = new MimeMessage();
-        message.From.Add(new MailboxAddress("PcForum","EmailPcForumOrganisation"));
+        try
+        {
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("PcForum","EmailPcForumOrganisation"));
         
-        message.To.Add(new MailboxAddress(user.Username,user.Email));
+            message.To.Add(new MailboxAddress(user.Username,user.Email));
 
-        message.Subject = "Velkommen til forummet";
+            message.Subject = "Velkommen til forummet";
 
-        var body = new TextPart("plain")
-        {
-            Text = "Velkommen til forummet " + user.Username +
-                   ". Vi glæder os til, at du tager del i vores community. Hilsen Moderator teamet."
-        };
+            var body = new TextPart("plain")
+            {
+                Text = "Velkommen til forummet " + user.Username +
+                       ". Vi glæder os til, at du tager del i vores community. Hilsen Moderator teamet."
+            };
 
-        message.Body = body;
+            message.Body = body;
 
-        using (var client = new SmtpClient())
-        {
-            client.Connect("smtp.gmail.com", 465, true);
+            using (var client = new SmtpClient())
+            {
+                client.Connect("smtp.gmail.com", 465, true);
             
-            client.Authenticate(Environment.GetEnvironmentVariable("fromemail"),Environment.GetEnvironmentVariable("frompass"));
-            client.Send(message);
-            client.Disconnect(true);
+                client.Authenticate(Environment.GetEnvironmentVariable("fromemail"),Environment.GetEnvironmentVariable("frompass"));
+                client.Send(message);
+                client.Disconnect(true);
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Failed to send" + e.Message);
+            throw new Exception("Could not send email");
         }
 
     }
